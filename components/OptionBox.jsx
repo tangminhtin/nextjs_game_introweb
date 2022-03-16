0cae8c254169849570ca828e3e1836607870ed6e
hoald20 <leduchoa911@gmail.com>
2022-03-16 12:51:40 +0700

    add donate

--- components/OptionBox.jsx
+++ components/OptionBox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "../styles/OptionBox.module.css";
 
 const OptionBox = () => {
-  return <div className={styles.option}></div>;
+  return <div className={styles.option}/>;
 };
 
 export default OptionBox;
--- components/Sponsor.jsx
+++ components/Sponsor.jsx
@@ -4,44 +4,161 @@ import OptionBox from "./OptionBox";
 import {Col, Container, Row} from "react-bootstrap";
 import customStyle from "../styles/Sponsor.module.css";
 import optionBoxStyle from "../styles/OptionBox.module.css"
+import Modal from "./Modal";
 
 const Sponsor = () => {
     return (
         <div className={styles.sponsor}>
-            <div className="heading">DONATE</div>
-            <button className="btn btn-primary mb-4" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                $5 Gói ủng hộ tinh thần
-            </button>
-            <div className="collapse" id="collapseExample">
-                <div className="card card-body">
-                    <b>Gói ủng hộ tinh thần</b>
-                    Đây là gói ủng hộ của những người thuần tuý muốn ủng hộ dự án và không yêu cầu hoàn lại tiền nếu
-                    việc gọi vống từ cộng đồng thất bại.
+            <div className={styles.box}>
+                <div className="heading" style={{textAlign: "center"}}>DONATE</div>
+                <div className={styles.button}>
+                    <button className="btn mb-4 mt-2 d-grid gap-2 w-100" type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#collapsevcb" aria-expanded="false"
+                            style={{
+                                background: "#307354",
+                                color: "white",
+                                boxShadow: "none"
+                            }}
+                    >
+                        $5 Gói ủng hộ tinh thần
+                    </button>
+                </div>
+
+
+                <div className="collapse" id="collapsevcb">
+                    <b style={{textAlign: "center"}}>Gói ủng hộ tinh thần</b> <br/>
+                    <div className={styles.text}>
+                        Đây là gói ủng hộ của những người thuần tuý muốn ủng hộ dự án và không yêu cầu hoàn lại tiền nếu
+                        việc gọi vống từ cộng đồng thất bại.
+                    </div>
+                </div>
+                <p>Số người đã ủng hộ tinh thần: 500</p>
+                <div className="row">
+                    <div className={"col-6"}>
+                        <img src={"img.png"} style={{width: "100%"}}/>
+
+                    </div>
+                    <div className={"col-6"}>
+                        <a className="btn btn-primary text-nowrap" role="button"
+                           aria-expanded="false" style={{backgroundColor: "#307354"}} onClick={Modal}>
+                            Ủng hộ ngay
+                        </a>
+
+                    </div>
                 </div>
             </div>
-            <a className="btn btn-primary mt-4" role="button"
-               aria-expanded="false" aria-controls="collapseExample">
-                Ủng hộ ngay: $5
-            </a>
-            {/*<Container style={{marginTop: "3rem"}} className={optionBoxStyle.option}>*/}
 
-            {/*    <div className="flex-row flex-wrap">*/}
-            {/*        <img src="TPB_HoaLD.png" alt="sponsor 1..." className={customStyle.image}/>*/}
-            {/*        <div className="card-footer w-100 text-muted" className={customStyle.text}>*/}
-            {/*            Buy me a coffee!*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
 
-            {/*</Container>*/}
-            {/*<OptionBox/>*/}
-            {/*<OptionBox/>*/}
-            {/*<OptionBox/>*/}
-            {/*<OptionBox/>*/}
-            {/*<OptionBox/>*/}
+            <div className={styles.box}>
+                <div className="heading" style={{textAlign: "center"}}>DONATE</div>
+                <button className="btn btn-outline-primary mb-4 mt-2 d-grid gap-2 w-100" type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#collapsetcb" aria-expanded="false"
+                        style={{
+                            background: "#307354",
+                            color: "white",
+                            boxShadow: "none"
+                        }}
 
+                >
+                    $5 Gói ủng hộ tinh thần
+                </button>
+
+                <div className="collapse" id="collapsetcb">
+                    <b style={{textAlign: "center"}}>Gói ủng hộ tinh thần</b> <br/>
+                    <div className="card card-body" className={styles.text}>
+                        Đây là gói ủng hộ của những người thuần tuý muốn ủng hộ dự án và không yêu cầu hoàn lại tiền nếu
+                        việc gọi vống từ cộng đồng thất bại.
+                    </div>
+                </div>
+                <p>Số người đã ủng hộ tinh thần: 500</p>
+                <div className="row">
+                    <div className={"col-6"}>
+                        <img src={"img_1.png"} style={{width: "100%"}}/>
+
+                    </div>
+                    <div className={"col-6"}>
+                        <a className="btn btn-primary text-nowrap" role="button"
+                           aria-expanded="false" style={{backgroundColor: "#307354"}}>
+                            Ủng hộ ngay
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.box}>
+                <div className="heading" style={{textAlign: "center"}}>DONATE</div>
+                <button className="btn btn-primary mb-4 mt-2 d-grid gap-2 w-100" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsebidv" aria-expanded="false"  style={{
+                    background: "#307354",
+                    color: "white",
+                    boxShadow: "none"
+                }}>
+                    $5 Gói ủng hộ tinh thần
+                </button>
+
+                <div className="collapse" id="collapsebidv">
+                    <b style={{textAlign: "center"}}>Gói ủng hộ tinh thần</b> <br/>
+                    <div className="card card-body" className={styles.text}>
+                        Đây là gói ủng hộ của những người thuần tuý muốn ủng hộ dự án và không yêu cầu hoàn lại tiền nếu
+                        việc gọi vống từ cộng đồng thất bại.
+                    </div>
+                </div>
+                <p>Số người đã ủng hộ tinh thần: 500</p>
+                <div className="row">
+                    <div className={"col-6"}>
+                        <img src={"img_2.png"} style={{width: "100%"}}/>
+
+                    </div>
+                    <div className={"col-6"}>
+                        <a className="btn btn-primary text-nowrap" role="button"
+                           aria-expanded="false" style={{backgroundColor: "#307354"}}>
+                            Ủng hộ ngay
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={styles.box}>
+                <div className="heading" style={{textAlign: "center"}}>DONATE</div>
+                <button className="btn btn-primary mb-4 mt-2 d-grid gap-2 w-100" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapsemb" aria-expanded="false"  style={{
+                    background: "#307354",
+                    color: "white",
+                    boxShadow: "none"
+                }}>
+                    $5 Gói ủng hộ tinh thần
+                </button>
+
+                <div className="collapse" id="collapsemb">
+                    <b style={{textAlign: "center"}}>Gói ủng hộ tinh thần</b> <br/>
+                    <div className="card card-body" className={styles.text}>
+                        Đây là gói ủng hộ của những người thuần tuý muốn ủng hộ dự án và không yêu cầu hoàn lại tiền nếu
+                        việc gọi vống từ cộng đồng thất bại.
+                    </div>
+                </div>
+                <p>Số người đã ủng hộ tinh thần: 500</p>
+                <div className="row">
+                    <div className={"col-6"}>
+                        <img src={"img_3.png"} style={{width: "100%"}}/>
+
+                    </div>
+                    <div className={"col-6"}>
+                        <a className="btn btn-primary text-nowrap" role="button"
+                           aria-expanded="false"  style={{backgroundColor: "#307354"}}>
+                            Ủng hộ ngay
+                        </a>
+
+                    </div>
+                </div>
+            </div>
         </div>
-    );
+
+    )
+        ;
 };
 
 export default Sponsor;
